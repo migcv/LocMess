0e8f4e8e37154fe0ce20d3cff4549fbcdbeb32c0
Miguel <migcviegas@gmail.com>
2017-03-31 22:01:17 +0100

    Implemented Map for PostLocation

--- app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/CreatePostActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/CreatePostActivity.java
@@ -1,18 +1,25 @@
 package pt.ulisboa.tecnico.cmov.locmess;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
+import java.util.Date;
+import java.util.Locale;
+
 public class CreatePostActivity extends AppCompatActivity {
+
+    EditText datetime = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,28 +54,62 @@ public class CreatePostActivity extends AppCompatActivity {
             }
         });
 
-        final EditText time = (EditText) findViewById(R.id.input_time);
-        time.setOnClickListener(new View.OnClickListener() {
+        datetime = (EditText) findViewById(R.id.input_time);
+        datetime.setKeyListener(null);
+        SimpleDateFormat dateF = new SimpleDateFormat("EEE, d MMM yyyy HH:mm", Locale.getDefault());
+        String date = dateF.format(Calendar.getInstance().getTime());
+        datetime.setText(date);
 
+        datetime.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Calendar myCalendar = Calendar.getInstance();
-                int hour = myCalendar.get(Calendar.HOUR_OF_DAY);
-                int minute = myCalendar.get(Calendar.MINUTE);
-                TimePickerDialog timePickerDialog = new TimePickerDialog(CreatePostActivity.this,
-                        new TimePickerDialog.OnTimeSetListener() {
-
-                            @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay,
-                                                  int minute) {
-
-                                time.setText(hourOfDay + ":" + minute);
-                            }
-                        }, hour, minute, false);
-                timePickerDialog.show();
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    pickTime();
+                }
             }
         });
+
+        datetime.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                pickTime();
+            }
+        });
+    }
+
+    private void pickTime() {
+        Calendar myCalendar = Calendar.getInstance();
+        int day = myCalendar.get(Calendar.DAY_OF_MONTH);
+        int month = myCalendar.get(Calendar.MONTH);
+        int year = myCalendar.get(Calendar.YEAR);
+        int hour = myCalendar.get(Calendar.HOUR_OF_DAY);
+        int minute = myCalendar.get(Calendar.MINUTE);
+
+        final TimePickerDialog timePickerDialog = new TimePickerDialog(CreatePostActivity.this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+                        datetime.setText(datetime.getText() + String.format(" %02d:%02d", hourOfDay, minute));
+                    }
+                }, hour, minute, true);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(CreatePostActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                          int dayOfMonth) {
+                        // TODO Auto-generated method stub
+                        SimpleDateFormat dateF = new SimpleDateFormat("EEE, d MMM yyyy");
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(year, monthOfYear, dayOfMonth);
+                        String date = dateF.format(calendar);
+                        datetime.setText(date);
+                        timePickerDialog.show();
+                    }
+                }, year, month, day);
+        datePickerDialog.show();
     }
 
 }
--- app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/LocationOptionActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/LocationOptionActivity.java
@@ -1,18 +1,49 @@
 package pt.ulisboa.tecnico.cmov.locmess;
 
+import android.Manifest;
+import android.animation.ObjectAnimator;
+import android.animation.TypeEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RadioButton;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
+import com.mapbox.mapboxsdk.annotations.PolygonOptions;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationSource;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.services.android.telemetry.location.LocationEngine;
+
+import java.util.ArrayList;
 
 public class LocationOptionActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private MapboxMap map;
+    private Marker marker ;
+    private LocationEngine locationEngine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_location_option);
 
         getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
@@ -41,6 +72,148 @@ public class LocationOptionActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        locationEngine = LocationSource.getLocationEngine(this);
+        locationEngine.activate();
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                marker = mapboxMap.addMarker(new MarkerViewOptions()
+                        .position(new LatLng(38.7378954, -9.1378972)));
+                mapboxMap.setOnMapClickListener(new MapboxMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(@NonNull LatLng point) {
+
+                        // When the user clicks on the map, we want to animate the marker to that
+                        // location.
+                        ValueAnimator markerAnimator = ObjectAnimator.ofObject(marker, "position",
+                                new LatLngEvaluator(), marker.getPosition(), point);
+                        markerAnimator.setDuration(250);
+                        markerAnimator.start();
+                    }
+                });
+                LatLng location = new LatLng(38.7378954, -9.1378972);
+                mapboxMap.addPolygon(new PolygonOptions().addAll(polygonCircleForCoordinate(location, 250.0)).fillColor(Color.parseColor("#4285F4")).alpha((float)0.4));
+                map = mapboxMap;
+            }
+        });
+
+        findViewById(R.id.radioButton_location).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setLayoutsGone();
+                findViewById(R.id.layout_location).setVisibility(View.VISIBLE);
+            }
+        });
+        findViewById(R.id.radioButton_wifi).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setLayoutsGone();
+                findViewById(R.id.layout_wifi).setVisibility(View.VISIBLE);
+            }
+        });
+        findViewById(R.id.radioButton_bluetooth).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setLayoutsGone();
+                findViewById(R.id.layout_bluetooth).setVisibility(View.VISIBLE);
+            }
+        });
+        findViewById(R.id.radioButton_wifi_direct).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setLayoutsGone();
+                findViewById(R.id.layout_wifi_direct).setVisibility(View.VISIBLE);
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    // Include method in your activity
+    private static class LatLngEvaluator implements TypeEvaluator<LatLng> {
+
+        private LatLng latLng = new LatLng();
+        @Override
+        public LatLng evaluate(float fraction, LatLng startValue, LatLng endValue) {
+            latLng.setLatitude(startValue.getLatitude() +
+                    ((endValue.getLatitude() - startValue.getLatitude()) * fraction));
+            latLng.setLongitude(startValue.getLongitude() +
+                    ((endValue.getLongitude() - startValue.getLongitude()) * fraction));
+            return latLng;
+        }
+    }
+
+    private ArrayList<LatLng> polygonCircleForCoordinate(LatLng location, double radius){
+        int degreesBetweenPoints = 8; //45 sides
+        int numberOfPoints = (int) Math.floor(360 / degreesBetweenPoints);
+        double distRadians = radius / 6371000.0; // earth radius in meters
+        double centerLatRadians = location.getLatitude() * Math.PI / 180;
+        double centerLonRadians = location.getLongitude() * Math.PI / 180;
+        ArrayList<LatLng> polygons = new ArrayList<>(); //array to hold all the points
+        for (int index = 0; index < numberOfPoints; index++) {
+            double degrees = index * degreesBetweenPoints;
+            double degreeRadians = degrees * Math.PI / 180;
+            double pointLatRadians = Math.asin(Math.sin(centerLatRadians) * Math.cos(distRadians) + Math.cos(centerLatRadians) * Math.sin(distRadians) * Math.cos(degreeRadians));
+            double pointLonRadians = centerLonRadians + Math.atan2(Math.sin(degreeRadians) * Math.sin(distRadians) * Math.cos(centerLatRadians),
+                    Math.cos(distRadians) - Math.sin(centerLatRadians) * Math.sin(pointLatRadians));
+            double pointLat = pointLatRadians * 180 / Math.PI;
+            double pointLon = pointLonRadians * 180 / Math.PI;
+            LatLng point = new LatLng(pointLat, pointLon);
+            polygons.add(point);
+        }
+        return polygons;
+    }
+
+    private void setLayoutsGone(){
+        findViewById(R.id.layout_location).setVisibility(View.GONE);
+        findViewById(R.id.layout_wifi).setVisibility(View.GONE);
+        findViewById(R.id.layout_bluetooth).setVisibility(View.GONE);
+        findViewById(R.id.layout_wifi_direct).setVisibility(View.GONE);
     }
 
 }
