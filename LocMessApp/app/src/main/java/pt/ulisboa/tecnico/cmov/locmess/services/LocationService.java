00a4ef320a3eeec4e298739a1a1fb580ec45d630
migcv <migcviegas@gmail.com>
2017-04-18 19:57:21 +0000

    Get Location...sometimes

--- LocMessApp/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/services/LocationService.java
+++ LocMessApp/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/services/LocationService.java
@@ -1,10 +1,24 @@
 package pt.ulisboa.tecnico.cmov.locmess.services;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresPermission;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.model.LatLng;
+import com.mapzen.android.lost.api.LocationServices;
 
 /**
  * Created by Miguel on 18/04/2017.
@@ -19,16 +33,35 @@ public class LocationService extends Service {
 
     @Override
     public void onCreate() {
-        Toast.makeText(this, "Service was Created", Toast.LENGTH_SHORT).show();
-        Log.d("SERVICE", "CREATED");
+        Log.d("LOCATION_ SERVICE", "Service created!");
+
         Thread thread = new Thread(new UserLocation());
-        thread.run();
+        thread.start();
     }
 
 
     public class UserLocation implements Runnable {
+
         public void run(){
-            System.out.println("MyRunnable running");
+            Log.d("LOCATION_ SERVICE", "Thread started!");
+            try {
+                LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+                Criteria criteria = new Criteria();
+                String provider = service.getBestProvider(criteria, false);
+
+                while(true) {
+                    try {
+                        Location location = service.getLastKnownLocation(provider);
+                        Log.d("LOCATION_ SERVICE", location == null ? "Null" : "" + location.getLatitude() + ", " + location.getLongitude());
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
         }
     }
 
