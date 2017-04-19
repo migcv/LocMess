2dda371f47b3680dee03aca408e1fbf5a96677b8
Miguel <migcviegas@gmail.com>
2017-04-19 15:56:05 +0100

    Location Service

--- LocMessApp/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/services/LocationService.java
+++ LocMessApp/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/services/LocationService.java
@@ -1,30 +1,25 @@
 package pt.ulisboa.tecnico.cmov.locmess.services;
 
-import android.Manifest;
 import android.app.Service;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.annotation.RequiresPermission;
-import android.support.v4.content.ContextCompat;
+import android.os.Looper;
 import android.util.Log;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.maps.model.LatLng;
-import com.mapzen.android.lost.api.LocationServices;
 
 /**
  * Created by Miguel on 18/04/2017.
  */
 
-public class LocationService extends Service {
+public class LocationService extends Service implements LocationListener {
+
+    Location location;
+    LocationManager locationManager;
+    double latitude;
+    double longitude;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -34,35 +29,100 @@ public class LocationService extends Service {
     @Override
     public void onCreate() {
         Log.d("LOCATION_ SERVICE", "Service created!");
-
         Thread thread = new Thread(new UserLocation());
         thread.start();
     }
 
 
     public class UserLocation implements Runnable {
-
         public void run(){
+            Looper.prepare();
+
             Log.d("LOCATION_ SERVICE", "Thread started!");
             try {
-                LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
-                Criteria criteria = new Criteria();
-                String provider = service.getBestProvider(criteria, false);
-
                 while(true) {
-                    try {
-                        Location location = service.getLastKnownLocation(provider);
-                        Log.d("LOCATION_ SERVICE", location == null ? "Null" : "" + location.getLatitude() + ", " + location.getLongitude());
-                        Thread.sleep(10000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    getLocation();
+                    Log.d("LOCATION_ SERVICE", location == null ? "Null" : "" + latitude + ", " + longitude);
+                    Thread.sleep(30000);
                 }
-
             } catch (SecurityException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    public Location getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            // getting GPS status
+            boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            // getting network status
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // no network provider is enabled
+            } else {
+                if (isNetworkEnabled) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            5000,
+                            10, this);
+                    Log.d("Network", "Network Enabled");
+                    if (locationManager != null) {
+                        location = locationManager
+                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                5000,
+                                10, this);
+                        Log.d("GPS", "GPS Enabled");
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return location;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
     }
 
 }
