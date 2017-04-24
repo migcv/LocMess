1fc5fd3bd97cd30dea806d3558c9e1e273234b04
Rafael-Barreira <rafael.spam.barreira@gmail.com>
2017-04-25 00:37:18 +0100

    Revert "Revert "Closes [#12], Closes [#4], Closes [#5]""
    This reverts commit 2c8143f99fcf908f746dbcd095b2d6b774f3a76a.

--- LocMessApp/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/activities/MyPostsActivity.java
+++ LocMessApp/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/activities/MyPostsActivity.java
@@ -3,6 +3,7 @@ package pt.ulisboa.tecnico.cmov.locmess.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 
@@ -33,6 +35,7 @@ import pt.ulisboa.tecnico.cmov.locmess.utils.SocketHandler;
 public class MyPostsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final int REQUEST_CREATE_POST = 0;
     ArrayList<List<String>> expandableMap;
     ArrayList<String> expandableListTitle;
     ExpandableListAdapter expandableListAdapter;
@@ -42,6 +45,17 @@ public class MyPostsActivity extends AppCompatActivity
         setContentView(R.layout.my_posts_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Intent intent = new Intent(getApplicationContext(), CreatePostActivity.class);
+                startActivityForResult(intent, REQUEST_CREATE_POST);
+            }
+        });
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
@@ -89,27 +103,7 @@ public class MyPostsActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.posts, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -126,8 +120,6 @@ public class MyPostsActivity extends AppCompatActivity
             startActivity(intent);
 
         } else if (id == R.id.nav_myposts) {
-
-        } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_logout) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
